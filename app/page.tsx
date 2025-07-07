2a00fe0cc2c3cd3066ae02a2b501671bb9e2215c
Sercan Atalik <sercan@atalik.com>
2025-07-07 20:25:57 +0100

    mnain page

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
     <div className="min-h-screen bg-background p-2 text-foreground">
       <div className="container">
         <h1 className="mb-5 text-3xl font-bold">
-         GDM Frontview
+         GDM FrontView Apps
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-8 gap-1">
-          <NavigationCard href="/credit" title="Credit" />
+        <div className="grid grid-cols-1 md:grid-cols-8 gap-4">
+          <NavigationCard  href="/credit" title="Credit"  />
           <NavigationCard href="/financing" title="Financing" />
         </div>
       </div>
